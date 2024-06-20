54525210525cae648efe5000c9f7bf88080d569c
jshao <zhou.shao0312@gmail.com>
2024-06-20 16:03:37 +1000

    feat: fetch graph data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,51 +7,22 @@ import axios from 'axios';
 import GridCanvas from './GridCanvas';
 
 function App() {
-    const [tableData, setTableData] = useState([]);
-
-    const fetchTableData = () => {
-        fetch('http://localhost:5000/api/TodoItems')
-            .then(response => response.json())
-            .then(data => setTableData(data))
-            .catch(error => console.error('Error fetching table data:', error));
-    };
-
     const [DijkstraData, setDijkstraData] = useState([]);
 
-    const fetchDijkstraData = () => {
-        axios.get('http://localhost:5000/api/Dijkstra')
-            .then(response => {
-                // Assuming response.data is a JSON object and you want to display a specific property
-                setDijkstraData(response.data); // Replace 'someProperty' with the actual property you want to display
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
+    const fetchDijkstraData = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/api/Dijkstra/GetNodes');
+            setDijkstraData(response.data);
+        } catch (error) {
+            console.error('Error fetching table data:', error);
+        }
     };
-
-    // const [message, setMessage] = useState('');
-
-    // useEffect(() => {
-    //     axios.get('http://localhost:5000/api/ProcessingTime')
-    //         .then(response => {
-    //             setMessage(response.data);
-    //         })
-    //         .catch(error => {
-    //             console.error('Error fetching data:', error);
-    //         });
-    // }, []);
 
     return (
         <div className="App">
-            <header className="App-header">
-                <h1>Shortest Path Algorithms Demo</h1>
-            </header>
             <main className='App-main'>
-                <GridCanvas />
+                <GridCanvas nodesData={DijkstraData.nodes} linesData={DijkstraData.graph}/>
                 <button className='fetch-button' onClick={fetchDijkstraData}>Dijkstra Algorithm</button>
-                <h1>{DijkstraData}</h1>
-                {/* <TableComponent data={tableData} />
-                <h1>{ message }</h1> */}
             </main>
         </div>
     );
